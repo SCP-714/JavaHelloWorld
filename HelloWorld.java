66631e22d32a81c048608349a5a6b0665d15f8d5
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2023-04-19 00:03:44 -0500

    Java nested for loop

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World!");
+        for(int i=1;i<=3;i++){
+        //loop of j
+            for(int j=1;j<=3;j++){
+                System.out.println(i+" "+j);
+            }//end of i
+        }//end of j
     }
 }
