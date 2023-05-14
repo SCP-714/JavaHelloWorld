64af7b3a982fe5be2df702002dafb8bc247f766c
SCP-714 <113054208+SCP-714@users.noreply.github.com>
2023-05-13 21:43:07 -0500

    reviewed java

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
         for(int i=1;i<=10;i++){
         //loop of j
             for(int j=1;j<=3;j++){
+
                 System.out.println(i+" "+j);
             }//end of i
         }//end of j
